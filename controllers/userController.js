0892e2be39ebda74a3859cfd0b455c1da50c2555
ddmilo <ddmilo1029@gmail.com>
2017-04-10 14:27:49 -0400

    Created Auth boilerplate,passwords are hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ var router = express.Router()
 var bodyParser = require('body-parser') //parses information from POST
 var User = require('../models/user.model.js');
 var methodOverride = require('method-override')
+var authHelper = require('../helpers/auth.js');
 
 // GET
 router.get('/', function indexAction(req, res) {
@@ -15,11 +16,16 @@ router.get('/', function indexAction(req, res) {
 
 
 //POST create user
-router.post('/', function createAction(req, res){
+router.post('/', authHelper.createSecure, function createAction(req, res){
   console.log('User created');
   console.log('body:', req.body);
 
-  var user = new User(req.body)
+  var user = new User({
+    email: req.body.email,
+    password_digest: res.hashedPassword,
+    username: req.body.username
+
+  })
 
 
 
